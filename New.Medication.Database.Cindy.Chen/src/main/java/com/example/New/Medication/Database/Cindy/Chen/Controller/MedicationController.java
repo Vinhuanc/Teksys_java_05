838815fece43c4dd48b14b975fc883d3a2fc739d
Vinhuanc <102390736+Vinhuanc@users.noreply.github.com>
2022-06-08 16:05:51 -0500

    Add files via upload

--- New.Medication.Database.Cindy.Chen/src/main/java/com/example/New/Medication/Database/Cindy/Chen/Controller/MedicationController.java
+++ New.Medication.Database.Cindy.Chen/src/main/java/com/example/New/Medication/Database/Cindy/Chen/Controller/MedicationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +62,16 @@ public class MedicationController {
 		return "Medications";
 	}
 
+	@GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+    
 	@RequestMapping("/AtorvastatinPage")
 	public String AtorvastatinPage() {
 		return "AtorvastatinPage";
